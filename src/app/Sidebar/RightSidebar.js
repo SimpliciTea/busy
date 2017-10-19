a29771ddb892f6082113ccb4bb96b0458d12edad
JM Myers <jm.myers@everbridge.com>
2017-10-19 16:38:17 -0400

    Update interesting people component to get interesting people from API
    * https://trello.com/c/Ag4M0Qlg/144-user-profile-intersting-people-box

--- src/app/Sidebar/RightSidebar.js
+++ src/app/Sidebar/RightSidebar.js
@@ -2,76 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
-import people from '../../helpers/people';
 
-import {
-  getIsAuthenticated,
-  getAuthenticatedUser,
-  getFollowingList,
-} from '../../reducers';
+import { getIsAuthenticated, getAuthenticatedUser } from '../../reducers';
 
 import InterestingPeople from '../../components/Sidebar/InterestingPeople';
 import StartNow from '../../components/Sidebar/StartNow';
 import SignUp from '../../components/Sidebar/SignUp';
 
-@connect(
-  state => ({
-    authenticated: getIsAuthenticated(state),
-    authenticatedUser: getAuthenticatedUser(state),
-    followingList: getFollowingList(state),
-  }),
-)
+@connect(state => ({
+  authenticated: getIsAuthenticated(state),
+  authenticatedUser: getAuthenticatedUser(state),
+}))
 export default class RightSidebar extends React.Component {
   static propTypes = {
     authenticated: PropTypes.bool.isRequired,
     authenticatedUser: PropTypes.shape().isRequired,
-    followingList: PropTypes.arrayOf(PropTypes.string).isRequired,
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      randomPeople: this.getRandomPeople(),
-    };
-  }
-
-  getRandomPeople = () => people
-    .reduce((res, item) => {
-      if (!this.props.followingList.includes(item)) {
-        res.push({ name: item });
-      }
-      return res;
-    }, [])
-    .sort(() => 0.5 - Math.random()).slice(0, 5);
-
-  handleRefreshInterestingPeople = () => this.setState({
-    randomPeople: this.getRandomPeople(),
-  });
-
   render() {
-    const InterestingPeopleWithData = () => (
-      <InterestingPeople
-        users={this.state.randomPeople}
-        onRefresh={this.handleRefreshInterestingPeople}
-      />
-    );
+    const { authenticated, authenticatedUser } = this.props;
 
     return (
-      (this.props.authenticated
-        ? <Switch>
-          <Route path="/@:name" component={InterestingPeopleWithData} />
+      <div>
+        {!authenticated && <SignUp />}
+        <Switch>
+          <Route
+            path="/@:name"
+            component={() => <InterestingPeople authenticatedUser={authenticatedUser} />}
+          />
           <Route
             path="/"
-            render={() =>
-              (<div>
-                {this.props.authenticatedUser.last_root_post === '1970-01-01T00:00:00' &&
-                <StartNow />
-                }
-                <InterestingPeopleWithData />
-              </div>)}
+            render={() => (
+              <div>
+                {authenticatedUser.last_root_post === '1970-01-01T00:00:00' && <StartNow />}
+                <InterestingPeople authenticatedUser={authenticatedUser} />
+              </div>
+            )}
           />
         </Switch>
-        : <SignUp />)
+      </div>
     );
   }
 }
--- src/components/Sidebar/InterestingPeople.js
+++ src/components/Sidebar/InterestingPeople.js
@@ -1,41 +1,103 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
+import _ from 'lodash';
 import User from './User';
+import Loading from '../../components/Icon/Loading';
 import './InterestingPeople.less';
+import steemAPI from '../../steemAPI';
 
-const InterestingPeople = ({ users, onRefresh }) =>
-  (<div className="InterestingPeople">
-    <div className="InterestingPeople__container">
-      <h4 className="InterestingPeople__title">
-        <i className="iconfont icon-group InterestingPeople__icon" />
-        {' '}
-        <FormattedMessage id="interesting_people" defaultMessage="Interesting People" />
-        <i
-          role="presentation"
-          onClick={onRefresh}
-          className="iconfont icon-refresh InterestingPeople__icon-refresh"
-        />
-      </h4>
-      <div className="InterestingPeople__divider" />
-      {users && users.map(user => <User key={user.name} user={user} />)}
-      <h4 className="InterestingPeople__more">
-        <Link to={'/latest-comments'}>
-          <FormattedMessage id="discover_more_people" defaultMessage="Discover More People" />
-        </Link>
-      </h4>
-    </div>
-  </div>);
+class InterestingPeople extends Component {
+  static propTypes = {
+    authenticatedUser: PropTypes.shape({
+      name: PropTypes.string,
+    }),
+  };
 
-InterestingPeople.propTypes = {
-  users: PropTypes.arrayOf(PropTypes.shape()),
-  onRefresh: PropTypes.func,
-};
+  static defaultProps = {
+    authenticatedUser: {
+      name: '',
+    },
+  };
 
-InterestingPeople.defaultProps = {
-  users: [],
-  onRefresh: () => {},
-};
+  state = {
+    users: [],
+    loading: true,
+    noUsers: false,
+  };
+
+  componentWillMount() {
+    const authenticatedUsername = this.props.authenticatedUser.name;
+    const usernameValidator = window.location.pathname.match(/@(.*)/);
+    const username = usernameValidator ? usernameValidator[1] : authenticatedUsername;
+    this.getBlogAuthors(username);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const diffAuthUser =
+      JSON.stringify(this.props.authenticatedUser) !== JSON.stringify(nextProps.authenticatedUser);
+
+    if (diffAuthUser) {
+      const usernameValidator = window.location.pathname.match(/@(.*)/);
+      const username = usernameValidator ? usernameValidator[1] : nextProps.authenticatedUser.name;
+      this.getBlogAuthors(username);
+    }
+  }
+
+  getUsersAccountDetails = (usersQuery = []) =>
+    steemAPI.getAccountsAsync(usersQuery).then((result) => {
+      const users = _.map(result, user => ({
+        ...user,
+        about: JSON.parse(user.json_metadata).profile.about,
+      }));
+      this.setState({
+        users,
+        loading: false,
+      });
+    });
+
+  getBlogAuthors = (username = '') => steemAPI.getBlogAuthorsAsync(username).then((result) => {
+    const users = _.sortBy(result, user => user[1]).reverse().slice(0, 3).map(user => user[0]);
+    if (users.length > 0) {
+      this.getUsersAccountDetails(users);
+    } else {
+      this.setState({
+        noUsers: true,
+      });
+    }
+  });
+
+  render() {
+    const { users, loading, noUsers } = this.state;
+
+    if (noUsers) {
+      return <div />;
+    }
+
+    if (loading) {
+      return <Loading />;
+    }
+
+    return (
+      <div className="InterestingPeople">
+        <div className="InterestingPeople__container">
+          <h4 className="InterestingPeople__title">
+            <i className="iconfont icon-group InterestingPeople__icon" />
+            {' '}
+            <FormattedMessage id="interesting_people" defaultMessage="Interesting People" />
+          </h4>
+          <div className="InterestingPeople__divider" />
+          {users && users.map(user => <User key={user.name} user={user} />)}
+          <h4 className="InterestingPeople__more">
+            <Link to={'/latest-comments'}>
+              <FormattedMessage id="discover_more_people" defaultMessage="Discover More People" />
+            </Link>
+          </h4>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default InterestingPeople;
